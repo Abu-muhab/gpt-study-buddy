3032766ecff63c32bc1dd8831f2f03dc2322df78
Abdulmalik Abubakar <abumuhab98@gmail.com>
2023-09-23 19:02:24 +0100

    get users bots post impl

--- gpt_study_buddy_server/src/bots/bots.controller.ts
+++ gpt_study_buddy_server/src/bots/bots.controller.ts
@@ -1,15 +1,19 @@
-import { Body, Controller, Post, UseFilters } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseFilters } from '@nestjs/common';
 import { HttpDomainExceptionFilter } from 'src/common/http_domain_exception_filter';
 import { BotsService } from './bots.service';
 import { BotDto, CreateBotRequest } from './bots.dto';
 import { User } from 'src/users/user.model';
 import { UserDecorator } from 'src/users/user.decorator';
-import { ApiCreatedResponse, ApiOperation } from '@nestjs/swagger';
+import { ApiCreatedResponse, ApiOperation, ApiResponse } from '@nestjs/swagger';
+import { BotsRepository } from './bots.repository';
 
 @Controller('bots')
 @UseFilters(HttpDomainExceptionFilter)
 export class BotsController {
-  constructor(private readonly botsService: BotsService) {}
+  constructor(
+    private readonly botsService: BotsService,
+    private readonly botsRepository: BotsRepository,
+  ) {}
 
   @Post()
   @ApiCreatedResponse({ type: BotDto })
@@ -25,5 +29,13 @@ export class BotsController {
         userId: user.id,
       }),
     );
+  }
+
+  @Get()
+  @ApiOperation({ summary: 'Get all bots for a user' })
+  @ApiResponse({ type: BotDto, status: 200 })
+  async getUserBots(@UserDecorator() user: User): Promise<BotDto[]> {
+    const bots = await this.botsRepository.getUserBots(user.id);
+    return bots.map((bot) => BotDto.fromDomain(bot));
   }
 }
